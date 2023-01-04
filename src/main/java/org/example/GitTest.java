46414016b40abf4c9029c9764b9809ffddff268f
yuan <yuan@163.com>
2023-01-04 15:55:57 +0800

    hot-fix test first commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("22222");
         System.out.println("333");
         System.out.println("hot fix");
+        System.out.println("hot-fix test");
     }
 }
